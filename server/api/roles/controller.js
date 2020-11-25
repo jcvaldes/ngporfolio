0747cb081abdc4cbb6e76a91209e4d812dbebe14
Juan Carlos Valdes <idevkingos@gmail.com>
2020-11-25 09:56:32 -0300

    arquitectura base

--- server/api/roles/controller.js
+++ server/api/roles/controller.js
@@ -1,19 +1,41 @@
-import db from '../../models'
-import { Sequelize } from '../../models'
+import db from '../../models';
+import { Sequelize } from '../../models';
 class RolesController {
   static Fetch(req, res) {
-    db.Role.findAndCountAll()
+    db.Role.findAndCountAll({
+      order: [['id', 'ASC']],
+    })
       .then((roles) => {
-        res.status(200).json(roles)
+        res.status(200).json(roles);
       })
       .catch((err) => {
-        res
-          .status(400)
-          .json({
-            message:
-              'Problemas al intentar conectarse a la base de datos' + err,
-          })
+        res.status(400).json({
+          message: 'Problemas al intentar conectarse a la base de datos' + err,
+        });
+      });
+  }
+  static FetchOne(req, res) {
+    const id = +req.params.id;
+    db.Role.findOne({
+      where: { id },
+    })
+      .then((role) => {
+        if (role === 0) {
+          res.status(404).json({
+            error: 'Not Found',
+          });
+        } else {
+          res.status(200).json({
+            ok: true,
+            role,
+          });
+        }
       })
+      .catch((err) => {
+        res.status(400).json({
+          message: 'Problemas al intentar conectarse a la base de datos' + err,
+        });
+      });
   }
   static Create(req, res) {
     db.Role.create(req.body)
@@ -21,40 +43,53 @@ class RolesController {
         res.status(200).json({
           ok: true,
           role,
-        })
+        });
       })
       .catch(Sequelize.ValidationError, (msg) => {
-        res.status(422).json({ message: msg.original.message })
+        res.status(422).json({ message: msg.original.message });
       })
       .catch((err) => {
         res
           .status(400)
-          .json({ message: 'RESPONSESDB_CONNECTION_ERROR.message' })
+          .json({ message: 'RESPONSESDB_CONNECTION_ERROR.message' });
+      });
+  }
+  static Update(req, res) {
+    db.Role.update(req.body, {
+      where: { id: req.body.id },
+    })
+      .then((role) => {
+        res.status(201).json({
+          ok: true,
+        });
       })
+      .catch((err) => {
+        res.status(400).json({ message: 'Registro no actualizado' });
+      });
   }
   static Delete(req, res) {
-    const id  = +req.params.id
+    const id = +req.params.id;
     db.Role.destroy({ where: { id } })
       .then((result) => {
         if (result === 0) {
           res.status(404).json({
             error: 'Registro no encontrado',
-          })
+          });
         } else {
           res.status(200).json({
             message: 'Registro eliminado',
-          })
+          });
         }
       })
       .catch(Sequelize.ValidationError, (msg) => {
-        res.status(422).json({ message: msg.original.message })
+        res.status(422).json({ message: msg.original.message });
       })
       .catch((err) => {
         res
           .status(400)
-          .json({ message: 'RESPONSESDB_CONNECTION_ERROR.message' })
-      })
+          .json({ message: 'RESPONSESDB_CONNECTION_ERROR.message' });
+      });
   }
 }
 
-export default RolesController
+export default RolesController;
--- server/api/roles/routes.js
+++ server/api/roles/routes.js
@@ -1,9 +1,11 @@
-import express from 'express'
-import RolesController from './controller'
-const app = express()
+import express from 'express';
+import RolesController from './controller';
+const app = express();
 
-app.get('/', RolesController.Fetch)
-app.post('/', RolesController.Create)
-app.delete('/:id', RolesController.Delete)
+app.get('/', RolesController.Fetch);
+app.get('/:id', RolesController.FetchOne);
+app.post('/', RolesController.Create);
+app.put('/', RolesController.Update);
+app.delete('/:id', RolesController.Delete);
 
-export default app
+export default app;
