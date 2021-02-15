db3e02d58ec63175a21c41f63c59c0636b092930
Juan Carlos Valdes <idevkingos@gmail.com>
2021-02-15 10:28:04 -0300

    arquitectura base

---
+++ portfolio/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { PortfolioModule } from './pages/portfolio/portfolio.module';
+const routes: Routes = [
+  // { path: 'home', component: HomeComponent },
+  { path: 'in/:username', component: PortfolioComponent },
+
+  // Siempre en ultimo lugar estas dos rutas
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [
+    PortfolioModule,
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ portfolio/src/app/shared/components/featured-projects/featured-projects.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { FeaturedProjectsComponent } from './featured-projects.component';
+
+describe('FeaturedProjectsComponent', () => {
+  let component: FeaturedProjectsComponent;
+  let fixture: ComponentFixture<FeaturedProjectsComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FeaturedProjectsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FeaturedProjectsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
